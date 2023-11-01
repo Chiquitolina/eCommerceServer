22022940dc2cad28fe1f50780de80a83ff426256
Alejandro <aleig.906@gmail.com>
2023-11-01 14:43:02 -0300

    port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import morgan from 'morgan'
 import router from './routes/payment.route.js';
 import cors from 'cors'
 
 const app = express()
-const PORT = 3000
 
 app.use(cors());
 app.use(express.json());
@@ -12,10 +10,7 @@ app.use(router)
 app.use(express.static('src/public'));
 
 
-app.listen(PORT, () => {
-    console.log('Escuchando el puerto 3000')
-    return {
-    status: 200,
-    }
+app.listen(3001, () => {
+    console.log('Escuchando el puerto 3001')
 })
 
