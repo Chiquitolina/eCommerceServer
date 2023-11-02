beb4af651ddd937c64c22c7143835daa1e625639
Alejandro <aleig.906@gmail.com>
2023-11-02 11:35:44 -0300

    config server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import morgan from 'morgan'
 import path from 'path'
 import router from './routes/payment.route.js';
@@ -6,12 +7,21 @@ import cors from 'cors'
 
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(morgan('dev'))
 
-app.use('/', express.static('src/public'));
 app.use(cors());
 app.use(express.json());
+
 app.use(router)
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
 
 app.listen(3000, () => {
     console.log('Escuchando el puerto 3000')
