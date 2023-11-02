ef101325d09f9b824e487194d2e9438c460c78c9
Alejandro <aleig.906@gmail.com>
2023-11-02 10:40:53 -0300

    como estaba

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import express from 'express';
+import morgan from 'morgan'
 import path from 'path'
 import router from './routes/payment.route.js';
 import cors from 'cors'
 
 const app = express()
 
+app.use(morgan('dev'))
+
 app.use('/', express.static('src/public'));
 app.use(cors());
 app.use(express.json());
 app.use(router)
 
-const STATIC = path.resolve('src/public');
-
-app.use(express.static(STATIC));
-
-app.get('*', (_, res) => {
-  res.sendFile(path.join('src/public', 'index.html'));
-});
-
 app.listen(3000, () => {
-    console.log('Escuchando el puerto 3001')
+    console.log('Escuchando el puerto 3000')
 })
 
