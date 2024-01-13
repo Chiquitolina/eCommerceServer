87aae7633d0619cda45281ce3a4caa8049029c54
Alejandro <aleig.906@gmail.com>
2024-01-13 12:34:45 -0300

    try/catch create order fn

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -18,26 +18,30 @@ export const createOrder = async (req, res) => {
     itemss.push(item);
   });
 
-  mercadopago.configure({
-    access_token:
-      "APP_USR-7164111176476079-103013-c07b7b3052b41805a443030050951190-1105995931",
-  });
+  try {
+    mercadopago.configure({
+      access_token:
+        "APP_USR-7164111176476079-103013-c07b7b3052b41805a443030050951190-1105995931",
+    });
 
-  const result = await mercadopago.preferences.create({
-    items: itemss,
-    back_urls: {
-      success: "http://localhost:3000/success",
-      failure: "http://localhost:3000/failure",
-      pending: "http://localhost:3000/pending",
-    },
-    notification_url: "https://379f-190-16-205-196.ngrok.io/web-hook",
-  });
+    const result = await mercadopago.preferences.create({
+      items: itemss,
+      back_urls: {
+        success: "http://localhost:3000/success",
+        failure: "http://localhost:3000/failure",
+        pending: "http://localhost:3000/pending",
+      },
+      notification_url: "https://379f-190-16-205-196.ngrok.io/web-hook",
+    });
 
-  console.log(result);
+    console.log(result);
 
-  console.log(itemss);
+    console.log(itemss);
 
-  res.json({ message: result.body.init_point });
+    res.json({ message: result.body.init_point });
+  } catch (error) {
+    console.log("Error:", error);
+  }
 };
 
 export const healthCheck = async (req, res) => {
