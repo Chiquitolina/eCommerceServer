086ddbb0f64a24eb64b88ab55206d3cf72d4cb98
Alejandro <aleig.906@gmail.com>
2024-01-15 23:07:46 -0300

    cors fixed?

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import morgan from 'morgan'
-import path from 'path'
-import router from './routes/payment.route.js';
-import cors from 'cors'
+import express from "express";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+import path from "path";
+import router from "./routes/payment.route.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(morgan('dev'))
+app.options("*", cors());
+
+app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
 
-app.use(router)
+app.use(router);
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(3000, () => {
-    console.log('Escuchando el puerto 3000')
-})
-
+  console.log("Escuchando el puerto 3000");
+});
