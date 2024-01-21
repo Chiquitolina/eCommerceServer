205fa4e855aeb713910aa7c77cbbe951ce8b91e5
Alejandro <aleig.906@gmail.com>
2024-01-21 10:35:01 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(morgan("dev"));
+
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://nora.com.ar"], // Orígenes permitidos
+  origin: [
+    "http://localhost:3000",
+    "http://nora.com.ar",
+    "http://localhost:4200",
+  ], // Orígenes permitidos
   methods: "GET,POST,PUT,DELETE,OPTIONS", // Métodos permitidos
   allowedHeaders: "Content-Type,Authorization", // Encabezados permitidos
 };
@@ -19,9 +25,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Habilita preflight para todas las rutas
 
-app.use(morgan("dev"));
-
-app.use(cors());
 app.use(express.json());
 
 app.use(router);
