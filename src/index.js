0c8d5523c939916432cf391c7b56ba29c6854723
Alejandro <aleig.906@gmail.com>
2024-01-21 11:05:59 -0300

    list 0000

--- src/index.js
+++ src/index.js
@@ -35,6 +35,6 @@ app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Escuchando el puerto 3000");
 });
