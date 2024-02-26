5f8e7f5395a7d335f9bf93db4b9f108bfe855010
Alejandro <aleig.906@gmail.com>
2024-02-25 23:10:24 -0300

    back

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,12 @@
 import jwt from "jsonwebtoken";
 import fs from "fs/promises";
-import path from "path";
 
 const SECRET_KEY = process.env.JWT_SECRET;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 const getUserCredentialas = async () => {
   try {
-
-    const productsPath = path.join(__dirname, "../data/creds.json");
-    const data = await fs.readFile(productsPath, "utf8");
-    return JSON.parse(data)    
+    const credentials = await fs.readFile("src/data/creds.json", "utf8");
+    return JSON.parse(credentials);
   } catch (error) {
     console.log("Error al traer las credenciales", error);
     throw error;
